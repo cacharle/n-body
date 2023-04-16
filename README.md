c14c76e30e4c8a51d57b1996c74ced494fb1dc1b
Charles Cabergs <me@cacharle.xyz>
2023-04-16 22:34:38 +0200

    Added fps counter

---
+++ README.md
@@ -0,0 +1,7 @@
+# n-body simulation
+
+## Benchmark
+
+| Setup                 | bodies at 30 fps  | commit                                   |
+|-----------------------|-------------------|------------------------------------------|
+| Naive approche O(n^1) | 2250              | 229be3e69391b89b08834bdac157ad53179b17ff |
--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
+#include "SDL_pixels.h"
+#include "SDL_render.h"
+#include "SDL_surface.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SDL_ASSERT_NO_ERROR                                                       \
     do                                                                            \
@@ -27,9 +32,10 @@ struct body
 static const uint32_t window_width = 1000;
 static const uint32_t window_height = 1000;
 static SDL_Renderer  *renderer = NULL;
+static const char    *font_path = "/usr/share/fonts/noto/NotoSansMono-SemiBold.ttf";
 // bodies
 static const double gravity = .000002;
-#define BODIES_COUNT 30
+#define BODIES_COUNT 2250
 static struct body bodies_previous[BODIES_COUNT] = {0};
 static struct body bodies[BODIES_COUNT] = {0};
 
@@ -124,6 +130,7 @@ main(void)
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_ASSERT_NO_ERROR;
+    TTF_Init();
     SDL_Window *window = SDL_CreateWindow("n-body",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
@@ -133,7 +140,10 @@ main(void)
     SDL_ASSERT_NO_ERROR;
     renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_ASSERT_NO_ERROR;
+    TTF_Font *font = TTF_OpenFont(font_path, 16);
 
+    struct timespec previous_time;
+    clock_gettime(CLOCK_MONOTONIC, &previous_time);
     bool running = true;
     while (running)
     {
@@ -155,11 +165,35 @@ main(void)
         SDL_RenderClear(renderer);
         update_bodies();
         draw_bodies();
+
+        // Compute and fps and display it
+        struct timespec current_time;
+        clock_gettime(CLOCK_MONOTONIC, &current_time);
+        long int time_difference = current_time.tv_nsec - previous_time.tv_nsec;
+        time_difference /= 1000000;
+        if (time_difference > 0)
+        {
+            long int fps = 1000 / time_difference;
+            char     buf[128] = {0};
+            snprintf(buf, 128, "%ld fps", fps);
+            SDL_Surface *surface =
+                TTF_RenderText_Solid(font, buf, (SDL_Color){100, 255, 100, 255});
+            SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+            SDL_FreeSurface(surface);
+            SDL_Rect r = {.x = 5, .y = 5};
+            SDL_QueryTexture(texture, NULL, NULL, &r.w, &r.h);
+            SDL_RenderCopy(renderer, texture, NULL, &r);
+            SDL_DestroyTexture(texture);
+        }
+        memcpy(&previous_time, &current_time, sizeof previous_time);
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(10);
     }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_CloseFont(font);
+    TTF_Quit();
     SDL_Quit();
     return EXIT_SUCCESS;
 }
